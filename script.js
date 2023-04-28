467fa796cbc008dfbf547fb7bdc692bb8684d7eb
Paul <pmacellaro7@aol.com>
2023-04-28 12:01:20 -0400

    formatting shtuff

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 const h2 = document.createElement("h2");
 h2.textContent = "This content added by JavaScript";
 document.querySelector("body").appendChild(h2);
+
+document.addEventListener("DOMContentLoaded", function() {
+    var navLinks = document.querySelectorAll("nav ul li a");
+  
+    navLinks.forEach(function(link) {
+      link.addEventListener("click", function(e) {
+        e.preventDefault();
+        console.log("You clicked on a nav link, I'm gnona assume it worked!");
+      });
+    });
+  });
